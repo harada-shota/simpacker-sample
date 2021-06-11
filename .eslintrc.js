5dd901b22bd95a14c1d533a8966c71d0aec979d5
Shota Harada <harada_shota@kakaku.com>
2021-06-11 09:27:44 +0900

    install eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  plugins: ["react-hooks", "react", "@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ["/vendor", "/public"],
+  rules: {
+    "react/prop-types": "off",
+  },
+};
